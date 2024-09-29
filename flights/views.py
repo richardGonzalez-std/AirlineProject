09ed50904feb25a97065f97f33e6f29a2a392b78
richardGonzalez-std <zambranoa998@gmail.com>
2024-09-29 16:28:42 -0400

    2do commit

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Flight,Airport
 # Create your views here.
+def index(request):
+    return render(request,"flights/index.html",{
+        "flights": Flight.objects.all()
+    })
+
+def flight(request,flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html",{
+        "flight":flight,
+        "passengers": flight.passengers.all()
+    })
+
+def airport(request):
+    code = Airport.objects.all()
+    return render(request,"flights/airport.html",{
+        "code": code
+    })
